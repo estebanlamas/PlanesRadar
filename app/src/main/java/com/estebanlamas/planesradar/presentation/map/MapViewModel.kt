6362f63309432b75bc49c24f66d2c22445b12347
Esteban Lamas <esteban.lamas@mo2o.com>
2021-02-14 23:01:43 +0100

    refactor

--- app/src/main/java/com/estebanlamas/planesradar/presentation/map/MapViewModel.kt
+++ app/src/main/java/com/estebanlamas/planesradar/presentation/map/MapViewModel.kt
@@ -1,11 +1,9 @@
 package com.estebanlamas.planesradar.presentation.map
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import com.estebanlamas.planesradar.domain.GetAircraftsUseCase
-import com.estebanlamas.planesradar.domain.model.Aircraft
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
@@ -13,13 +11,11 @@ import javax.inject.Inject
 /**
  * Created by esteban on 24/9/17
  */
-
 class MapViewModel
 @Inject constructor(getAircraftsUseCase: GetAircraftsUseCase): ViewModel() {
-    private val _error = MutableLiveData<String>()
-    val error: LiveData<String> = _error
-    val aircraftList: LiveData<List<Aircraft>> = getAircraftsUseCase.execute()
-        .catch { _error.postValue(it.message) }
+    val error = MutableLiveData<String>()
+    val aircraftList = getAircraftsUseCase.execute()
+        .catch { error.postValue(it.message) }
         .map { it.aircrafts }
         .asLiveData()
 }
